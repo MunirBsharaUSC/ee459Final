85ae165b3e1aa48750679930977a6d09b79752c2
Parad0xed <etzhong@usc.edu>
2024-04-20 17:24:57 -0700

    Implemented lcd_print() for streamlined printing

--- lcd.c
+++ lcd.c
@@ -6,13 +6,15 @@ void lcd_send_command(uint8_t command) {
 
     // Send the command using i2c_io; adjust the device_addr as necessary
     i2c_io(ADDR2, command_packet, 2, NULL, 0);
+
+    _delay_ms(15);
 }
 
 void lcd_send_data(char *command) {
     uint8_t control_byte = 0x40; // Control byte to indicate a command (adjust based on your LCD's protocol)
     uint8_t command_packet[strlen(command)+1];
-    command_packet[0]=control_byte;
-    uint8_t i=strlen(command);
+    command_packet[0] = control_byte;
+    uint8_t i = strlen(command);
 
     while(i!=0){
         command_packet[strlen(command)-i+1]=command[strlen(command)-i];
@@ -21,6 +23,8 @@ void lcd_send_data(char *command) {
 
     // Send the command using i2c_io; adjust the device_addr as necessar
     i2c_io(ADDR2, command_packet, strlen(command)+1, NULL, 0);
+
+    _delay_us(120);
 }
 
 void lcd_init(void) {
@@ -42,3 +46,64 @@ void lcd_init(void) {
     lcd_send_command(LCD_ENTRY_MODE_SET);
     _delay_us(120); // Wait for command to execute
 }
+
+// Function to directly print output strings to the LCD
+void lcd_print(char* output, uint8_t row){
+    char row_out[21];
+    memset(row_out, ' ', 21);
+
+    if(row >= 1 && row <=4){ // Print to a specific row (truncates to 20 characters)
+        if (row == 1)
+            lcd_send_command(LCD_MOVE_ROW1);
+        else if(row == 2)
+            lcd_send_command(LCD_MOVE_ROW2);
+        else if (row == 3)
+            lcd_send_command(LCD_MOVE_ROW3);
+        else if (row == 4)
+            lcd_send_command(LCD_MOVE_ROW4);
+
+        uint8_t i;
+        for(i=0; i<20; i++)
+            row_out[i] = output[i];
+        row_out[20] = '\0';
+        lcd_send_data(row_out);
+    }
+    else{// Print to entire display (truncates to 80 characters with proper wrapping)
+        uint8_t i = 0;
+        
+        // Print row 1
+        lcd_send_command(LCD_MOVE_ROW1);
+        for(i=0; i<20; i++)
+            row_out[i] = output[i];
+        row_out[20] = '\0';
+        lcd_send_data(row_out);
+
+        // Print row 2
+        if(strlen(output) > 20){
+            lcd_send_command(LCD_MOVE_ROW2);
+            for(i=0; i<20; i++)
+                row_out[i] = output[20+i];
+            row_out[20] = '\0';
+            lcd_send_data(row_out);
+        }
+
+        // Print row 3
+        if(strlen(output) > 40){
+            lcd_send_command(LCD_MOVE_ROW3);
+            for(i=0; i<20; i++)
+                row_out[i] = output[40+i];
+            row_out[20] = '\0';
+            lcd_send_data(row_out);
+        }
+
+        // Print row 4
+        if(strlen(output) > 60){
+            lcd_send_command(LCD_MOVE_ROW4);
+            for(i=0; i<20; i++)
+                row_out[i] = output[60+i];
+            row_out[20] = '\0';
+            lcd_send_data(row_out);
+        }
+    }
+}
+
