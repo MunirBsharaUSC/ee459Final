87868ec9e36e7f7ceb58537fbdb7f8aa452ab99a
‘MunirBsharaUSC’ <bshara@usc.edu>
2024-04-21 19:50:01 -0700

    main updated for accel heartrate

--- main.c
+++ main.c
@@ -30,8 +30,21 @@ int main(void) {
 
     // Variables and Buffers
     char accel_buf[20];
-    int16_t x, y, z;
+    int16_t z;
 
+    uint8_t delayTime=0;
+    uint8_t delayFlag=0;
+    unsigned long step=0;
+
+    unsigned long count=0;
+    unsigned long beat_times[10]={0};
+    uint8_t currIndex=0;
+    uint8_t startIndex=0;
+    uint8_t en=0;
+    uint8_t prevSample=0;
+
+
+    z=0;
     // Display succesful boot
     _delay_ms(1000);
     lcd_clear(3);
@@ -42,7 +55,7 @@ int main(void) {
 
     // Main Program Loop (accelerometer test)
     while(1){
-        accel_read(&x, &y, &z);
+        /*accel_read(&x, &y, &z);
         lcd_print("ACCELEROMETER", 1);
         snprintf(accel_buf, 20, "X:%d", x);
         lcd_clear(2);
@@ -52,9 +65,18 @@ int main(void) {
         lcd_print(accel_buf, 3);
         snprintf(accel_buf, 20, "Z:%d", z);
         lcd_clear(4);
+        lcd_print(accel_buf, 4);*/
+        pedometer(&z, &delayTime, &delayFlag, &step);
+        snprintf(accel_buf, 20, "Y:%ld", step);
+        lcd_clear(4);
         lcd_print(accel_buf, 4);
 
-        _delay_ms(100);
+        heartbeatCalc(accel_buf, &count, beat_times, &currIndex, &startIndex, &en, &prevSample);
+        if(count==1){
+            lcd_clear(3);
+            lcd_print(accel_buf, 3);
+        }
+        _delay_ms(10);
     };
 
     return 0;   // Never reached
